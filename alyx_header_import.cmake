8046333aea340cf9072a7838d7c5ecbb1d08c957
Alex <130048160+bauer-alex-174@users.noreply.github.com>
2024-01-02 08:31:15 +0100

    Update alyx_header_import.cmake

--- alyx_header_import.cmake
+++ alyx_header_import.cmake
@@ -3,9 +3,6 @@ if (DEFINED ENV{ALYX_HEADER_PATH} AND (NOT ALYX_HEADER_PATH))
     message("Using ALYX_HEADER_PATH from environment ('${ALYX_HEADER_PATH}')")
 endif ()
 
-# TO DO: 
-# Change Variable names
-# Change Repo details
 if (DEFINED ENV{ALYX_HEADER_FETCH_FROM_GIT} AND (NOT ALYX_HEADER_FETCH_FROM_GIT))
     set(ALYX_HEADER_FETCH_FROM_GIT $ENV{ALYX_HEADER_FETCH_FROM_GIT})
     message("Using ALYX_HEADER_FETCH_FROM_GIT from environment ('${ALYX_HEADER_FETCH_FROM_GIT}')")
@@ -29,7 +26,7 @@ if (NOT ALYX_HEADER_PATH)
                 GIT_TAG main
         )
         if (NOT alyx_header)
-            message("Downloading Raspberry Pi Pico Extras")
+            message("Downloading Alyx C Headers ...")
             FetchContent_Populate(alyx_header)
             set(ALYX_HEADER_PATH ${alyx_header_SOURCE_DIR})
         endif ()
@@ -40,21 +37,21 @@ if (NOT ALYX_HEADER_PATH)
             message("Defaulting ALYX_HEADER_PATH as sibling of PICO_SDK_PATH: ${ALYX_HEADER_PATH}")
         else()
             message(FATAL_ERROR
-                    "PICO EXTRAS location was not specified. Please set ALYX_HEADER_PATH or set ALYX_HEADER_FETCH_FROM_GIT to on to fetch from git."
+                    "Alyx C Header location was not specified. Please set ALYX_HEADER_PATH or set ALYX_HEADER_FETCH_FROM_GIT to on to fetch from git."
                     )
         endif()
     endif ()
 endif ()
 
 set(ALYX_HEADER_PATH "${ALYX_HEADER_PATH}" CACHE PATH "Path to the PICO EXTRAS")
-set(ALYX_HEADER_FETCH_FROM_GIT "${ALYX_HEADER_FETCH_FROM_GIT}" CACHE BOOL "Set to ON to fetch copy of PICO EXTRAS from git if not otherwise locatable")
-set(ALYX_HEADER_FETCH_FROM_GIT_PATH "${ALYX_HEADER_FETCH_FROM_GIT_PATH}" CACHE FILEPATH "location to download EXTRAS")
+set(ALYX_HEADER_FETCH_FROM_GIT "${ALYX_HEADER_FETCH_FROM_GIT}" CACHE BOOL "Set to ON to fetch copy of Alyx C Headers from git if not otherwise locatable")
+set(ALYX_HEADER_FETCH_FROM_GIT_PATH "${ALYX_HEADER_FETCH_FROM_GIT_PATH}" CACHE FILEPATH "location to download Alyx C Headers")
 
 get_filename_component(ALYX_HEADER_PATH "${ALYX_HEADER_PATH}" REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
 if (NOT EXISTS ${ALYX_HEADER_PATH})
     message(FATAL_ERROR "Directory '${ALYX_HEADER_PATH}' not found")
 endif ()
 
-set(ALYX_HEADER_PATH ${ALYX_HEADER_PATH} CACHE PATH "Path to the PICO EXTRAS" FORCE)
+set(ALYX_HEADER_PATH ${ALYX_HEADER_PATH} CACHE PATH "Path to the Alyx C Headers" FORCE)
 
-add_subdirectory(${ALYX_HEADER_PATH} pico_extras)
+add_subdirectory(${ALYX_HEADER_PATH} alyx_header)
